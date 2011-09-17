54e706d1b99075a88ca73bdda1347dd91a3d6432
Josh Durgin <josh.durgin@dreamhost.com>
2011-09-16 17:00:45 -0700

    uninstall: remove qemu files before removing the package and fix package list

--- uninstall.sh
+++ uninstall.sh
@@ -6,15 +6,16 @@ if [ ! -f $DIR/previous_packages.txt ]; then
 	echo "No previous_packages.txt so we can't tell what to uninstall!"
 	exit 1
 fi
+
+# remove custom qemu files
+sudo rm -f /usr/bin/qemu-system-x86_64
+sudo rm -rf /usr/share/qemu
 # We assume that we did not remove any packages
 sudo dpkg --get-selections > $DIR/current_packages.txt
-NEW_PKGS=`diff -u previous_packages.txt current_packages.txt | grep '^+' | cut -f 1 | cut -c 2- | tr '\n' ' '`
-sudo apt-get remove $NEW_PKGS || echo "failed to remove packages" && exit 1
+NEW_PKGS=`diff -u previous_packages.txt current_packages.txt | grep '^+[^+]' | cut -f 1 | cut -c 2- | tr '\n' ' '`
+sudo apt-get remove $NEW_PKGS
 sudo rm -f /etc/ceph/ceph.conf
 sudo rm -f /etc/apt/sources.list.d/ceph.list
 sudo rm -rf ~/ceph_logs 
 sudo rm -rf ~/openstack
-rm -f $DIR/debian.img
 rm -rf $DIR/qemu-kvm
-# remove custom qemu
-sudo rm /usr/bin/qemu-system-x86_64
